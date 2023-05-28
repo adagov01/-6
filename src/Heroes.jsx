691a18654edb0170ac0e361f0d9ca6022c106a7a
unknown <adagov.abdul.095@mail.ru>
2023-05-29 02:03:08 +0300

    first commit

---
+++ src/Heroes.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function Heroes(){
+
+    const [names, setNames] = useState([
+        "Шерлок Холмс", 
+        "Доктор Ватсон", 
+        "Профессор Мориарти",
+        "Миссис Хадсон",
+        "Ирен Адлер"
+    ])
+
+    return(
+        <div>
+            {
+                names.map((name, index) =>
+                <li key={index}>{name}</li>
+            )
+            }
+        </div>
+    )
+}
+
+export default Heroes;
